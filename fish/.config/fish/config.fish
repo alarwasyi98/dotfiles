778634fc727d7dbea3183353d6079ee279e13f84
alarwasyi98 <alarwasyi98@gmail.com>
2024-09-12 19:50:10 +0700

    feat: add fish shell configuration
    Hyprland Startup file, FISH_VAR, FISH_CONFIG.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,103 @@
+#        .__    
+# _____  |  |   
+# \__  \ |  |     Abdul Hakim (alarwasyi98)
+#  / __ \|  |__   https://github.com/alarwasyi98
+# (____  /____/ 
+#      \/       
+
+### EXPORTS ###
+set fish_greeting # Supresses fish's intro message
+set TERM xterm-256color # Sets the terminal type
+
+### I DONT KNOW WHAT IT IS ##
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+end
+
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
+end
+
+## END ##
+
+
+### ALIASES ###
+alias cls='clear'
+alias efish=' nvim ~/.config/fish/config.fish'
+
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# alias chmod commands
+alias mx='chmod a+x'
+alias 000='chmod -R 000'
+alias 644='chmod -R 644'
+alias 666='chmod -R 666'
+alias 755='chmod -R 755'
+alias 777='chmod -R 777'
+
+# Changing "ls" to "eza"
+alias ls='eza -al --color=always --icons=always --git --group-directories-first' # my preferred listing
+alias la='eza -a --color=always --icons=always --git --group-directories-first' # all files and dirs
+alias ll='eza -l --color=always --icons=always --git --group-directories-first' # long format
+alias lt='eza -aT --color=always --icons=always --git --group-directories-first' # tree listing
+alias l.='eza -a | egrep "^\."'
+alias l.='eza -al --color=always --icons=always --group-directories-first ../' # ls on the PARENT directory
+alias l..='eza -al --color=always --icons=always --group-directories-first ../../' # ls on directory 2 levels up
+alias l...='eza -al --color=always --icons=always --group-directories-first ../../../' # ls on directory 3 levels up
+
+# pacman and yay
+alias pacsyu='sudo pacman -Syu' # update only standard pkgs
+alias pacsyyu='sudo pacman -Syyu' # Refresh pkglist & update standard pkgs
+alias parsua='paru -Sua --noconfirm' # update only AUR pkgs (paru)
+alias parsyu='paru -Syu --noconfirm' # update standard pkgs and AUR pkgs (paru)
+alias unlock='sudo rm /var/lib/pacman/db.lck' # remove pacman lock
+alias orphan='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages (DANGEROUS!)
+
+# adding flags
+alias df='df -h' # human-readable sizes
+alias free='free -m' # show sizes in MB
+alias grep='grep --color=auto' # colorize output (good for log files)
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias tag='git tag'
+alias newtag='git tag -a'
+alias gla='git log --oneline --graph --all'
+
+# fzf
+alias cdf='cd $(fd -t d | fzf)' # fd-find and fzf needs to be preinstalled
+alias fvim='nvim $(fzf --preview="bat --color=always {}" --bind shift-up:preview-page-up,shift-down:preview-page-down)' # nvim, bat and fzf needs to be preinstalled
+# alias ff='fzf --preview=less --bind shift-up:preview-page-up,shift-down:preview-page-down)'
+
+### SHOW COLORSCRIPT ON STARTUP ###
+colorscript --exec alpha
+
+### SETUP STARSHIP PROMPT ###
+starship init fish | source
+
+### SETUP ZOXIDE ###
+zoxide init fish | source
+
+### FUCTIONS ###
