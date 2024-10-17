ad0a3c7f5018935f09f3ecc307afb4ed214a73ed
Abdul Hakim <alarwasyi98@gmail.com>
2024-10-18 00:54:01 +0700

    chore: change default color to cyberdream

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,31 @@
 return {
-	-- add dracula
-	{ "Mofiqul/dracula.nvim" },
-
-	-- Configure LazyVim to load dracula
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = function(_, opts)
+			opts.transparent = true
+			opts.italic_comments = true
+			opts.borderless_telescope = false
+		end,
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "dracula",
+			colorscheme = "cyberdream",
 		},
+	},
+
+	-- modicator (auto color line number based on vim mode)
+	{
+		"mawkler/modicator.nvim",
+		dependencies = "scottmckendry/cyberdream.nvim",
+		init = function()
+			-- These are required for Modicator to work
+			vim.o.cursorline = false
+			vim.o.number = true
+			vim.o.termguicolors = true
+		end,
+		opts = {},
 	},
 }
